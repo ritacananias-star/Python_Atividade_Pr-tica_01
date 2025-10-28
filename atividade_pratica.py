620df200cda51aa0a94ea1d1bb018505351ad922
ritacananias-star <ritac.ananias@gmail.com>
2025-10-28 06:26:29 -0300

    Arquivo único com todas as questões da atividade prática

---
+++ atividade_pratica.py
@@ -0,0 +1,44 @@
+
+# ----------------------------------------
+# 1. Programa de Saudação
+# ----------------------------------------
+print("Hello, world!")
+
+# ----------------------------------------
+# 2. Calculadora de Soma
+# ----------------------------------------
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("Resultado da soma:", soma)
+
+# ----------------------------------------
+# 3. Calculadora de Volume
+# ----------------------------------------
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print("Volume da caixa:", volume, "cm³")
+
+# ----------------------------------------
+# 4. Calculadora de Preço Total
+# ----------------------------------------
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
+
+# ----------------------------------------
+# 5. Calculadora de Número Inteiro
+# ----------------------------------------
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+diferenca = (A * B - C * D)
+print("DIFERENCA =", diferenca)
